5ef022d401457ac2c0710d10f109f8901167a1bc
Tanja Dumanovic <dumanovicctanjaa@example.com>
2026-08-25 02:14:47 +0200

    Initial commit: Application code, Dockerfile and K8s manifests v1.0.0

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.json({
+    status: "success",
+    message: "Dobrodosli na FON Cloud GitOps demo aplikaciju!",
+    version: "v1.0.0",
+    author: "Tanja Dumanovic"
+  });
+});
+
+app.get('/health', (req, res) => {
+  res.status(200).send("OK");
+});
+
+app.listen(PORT, () => {
+  console.log(`Aplikacija slusa na portu ${PORT}`);
+});
